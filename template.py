c2af7e7ba851ead2f9bdba1574b6b16e6b25bb38
Anson <git@anson-cheung.com>
2019-10-09 00:20:07 +0100

    Add placeholder

--- template.py
+++ template.py
@@ -1,5 +1,5 @@
 from discord.ext import commands
-from tinydb import TinyDB
+from tinydb import TinyDB, Query
 
 
 class Template(commands.Cog):
@@ -7,6 +7,30 @@ class Template(commands.Cog):
         self.version = "1.0.0"
         self.bot = bot
         self.db = TinyDB('./modules/databases/template')
+
+    @commands.group(invoke_without_command=True)
+    async def template(self, ctx, name: str):
+        target = Query()
+        if not self.db.get(target.temp == name):
+            await ctx.send("[!] This template does not exist.")
+            return
+        # TODO: Implement it
+        return
+
+    @template.command(name="new")
+    async def new(self, ctx, name: str, *, content: str):
+        # TODO: Implement it
+        pass
+
+    @template.command(name="edit")
+    async def edit(self, ctx, name: str, *, content: str):
+        # TODO: Implement it
+        pass
+
+    @template.command(name="remove")
+    async def remove(self, name: str):
+        # TODO: Implement it
+        pass
 
 
 def setup(bot):
